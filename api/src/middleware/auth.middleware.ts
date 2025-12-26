aca2f3f30d943244f2e4a0906decbe16d7c523d8
pm-8 <mishraprakhar150@gmail.com>
2025-12-27 03:45:52 +0530

    changes made

--- api/src/middleware/auth.middleware.ts
+++ api/src/middleware/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { type Request, type Response,  NextFunction } from 'express';
+import { type Request, type Response, type NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { UserRole } from '../models/user.model';
 
